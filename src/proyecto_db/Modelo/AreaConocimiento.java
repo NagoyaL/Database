40842fbf4f82ffeb2170a1b7010b52e3e1abce5a
JuanPabloAnteSuarez03 <115759855+JuanPabloAnteSuarez03@users.noreply.github.com>
2023-06-02 01:05:08 -0500

    Add files via upload

--- src/proyecto_db/Modelo/AreaConocimiento.java
+++ src/proyecto_db/Modelo/AreaConocimiento.java
@@ -4,10 +4,54 @@
  */
 package proyecto_db.Modelo;
 
-/***
+/**
  *
  * @author gurui
  */
 public class AreaConocimiento {
-    
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private AreaConocimiento areaPadre; 
+
+    // Constructor
+    public AreaConocimiento(int codigo, String nombre, String descripcion, AreaConocimiento areaPadre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.areaPadre = areaPadre;
+    }
+
+    // Getters y Setters
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public AreaConocimiento getAreaPadre() {
+        return areaPadre;
+    }
+
+    public void setAreaPadre(AreaConocimiento areaPadre) {
+        this.areaPadre = areaPadre;
+    }
 }
--- src/proyecto_db/Modelo/Prestamo.java
+++ src/proyecto_db/Modelo/Prestamo.java
@@ -3,11 +3,77 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto_db.Modelo;
+import java.util.Date;
 
 /**
  *
  * @author gurui
  */
+
 public class Prestamo {
-    
+    private int numero;
+    private Date fecha;
+    private Date fechaDevolucion;
+    private Usuario usuario;
+    private Empleado empleado;
+    private Ejemplar ejemplar;
+
+    // Constructor
+    public Prestamo(int numero, Date fecha, Date fechaDevolucion, Usuario usuario, Empleado empleado, Ejemplar ejemplar) {
+        this.numero = numero;
+        this.fecha = fecha;
+        this.fechaDevolucion = fechaDevolucion;
+        this.usuario = usuario;
+        this.empleado = empleado;
+        this.ejemplar = ejemplar;
+    }
+
+    // Getters y Setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(Date fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(Ejemplar ejemplar) {
+        this.ejemplar = ejemplar;
+    }
 }
